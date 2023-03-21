ed6db7c749f4c9696ff12cfd35eae6fcc6868807
Chris Yoxall <chris.yoxall@gmail.com>
2023-03-21 22:11:30 +1300

    Adding .NET application that uses managed identity

---
+++ dot_net/managed.identity.demo/Program.cs
@@ -0,0 +1,35 @@
+﻿using Azure.Identity;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+const string storageAccountName = "chrisidentitydemosa";    // Name of storage account
+const string container = "chris-identity-demo-blob";        // Name of file share in the blob container
+const string blob = "demo.csv";                             // Name of blob
+
+const string blobEndpoint = $"https://{storageAccountName}.blob.core.windows.net/{container}/{blob}";
+
+// The Managed Identity ClientID. Needs to be granted appropriate access such as 'Storage Blob Data Contributor'
+// on the storage account.
+const string managedIdentityId = "7fbee3a4-c13b-4131-af4f-ea2c0ec99346";
+
+// From https://learn.microsoft.com/en-us/azure/storage/blobs/storage-quickstart-blobs-dotnet
+//
+//    - BlobServiceClient: The BlobServiceClient class allows you to manipulate Azure Storage resources and blob containers.
+//    - BlobContainerClient: The BlobContainerClient class allows you to manipulate Azure Storage containers and their blobs.
+//    - BlobClient: The BlobClient class allows you to manipulate Azure Storage blobs.
+
+// DefaultAzureCredential will use credentials of person running code when developing locally. Need to be logged
+// in (via az account login) and have access to blob. Will use the managed identity when running in Azure.
+var blobClient = new BlobClient(new Uri(blobEndpoint),
+    new DefaultAzureCredential(new DefaultAzureCredentialOptions { ManagedIdentityClientId = managedIdentityId}));
+
+using (var stream = blobClient.OpenRead(new BlobOpenReadOptions(false)))
+using (var reader = new StreamReader(stream))
+{
+    while (!reader.EndOfStream)
+    {
+        Console.WriteLine(reader.ReadLine());
+    }
+}
+
+Console.WriteLine($"{Environment.NewLine}Done!");
---
+++ dot_net/managed.identity.demo/README.md
@@ -0,0 +1,22 @@
+
+To run this demo on managed identities on a VM will need to assign a managed identity to the VM either at creation or afterwards by doing:
+
+    az vm identity assign -g chris-test-rg -n chris-test-vm --identities chris-identity-demo
+
+When running locally it will use your currently loged in AZ CLI account.
+
+
+
+The application will read a file out of a blob storage container.  Look at the code for currently configured details. As well as asiging
+the managed identity to the VM will need to add it's clientId in the code. The managed identity will need access granted on the storage
+account.
+
+
+
+To install .NET SDK and ASP.NET Core runtime on VM do: sudo apt-get update && sudo apt-get install -y dotnet-sdk-6.0 aspnetcore-runtime-6.0
+
+
+
+To copy the application to the new VM it will be something like (copying a file called archive.tar.gz to /home/azureuser on the VM):
+
+    scp -i ./id_rsa2  ~/Code/dotnet.colsole.app/dotnet.colsole.app/bin/Debug/net6.0/achive.tar.gz azureuser@20.213.93.100:/home/azureuser
