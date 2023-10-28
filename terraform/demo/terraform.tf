57790263d02945dc21243c08b9e21623a195742b
Chris Yoxall <chris.yoxall@gmail.com>
2023-10-28 15:20:53 +1300

    Example of for expression

--- terraform/demo/terraform.tf
+++ terraform/demo/terraform.tf
@@ -21,7 +21,7 @@ terraform {
   }
 }
 
-////* Output an item in a list *////
+////* Output an item from a list *////
 
 variable "candy_list" {
   type    = list(string)
@@ -69,9 +69,9 @@ output "http_status_code" {
   value = data.http.example.status_code
 }
 
-locals {    
-    http_response_values = jsondecode(data.http.example.response_body)
-    version = lookup(local.http_response_values, "current_version", "")
+locals {
+  http_response_values = jsondecode(data.http.example.response_body)
+  version              = lookup(local.http_response_values, "current_version", "")
 }
 
 output "latest_terraform_version" {
@@ -87,4 +87,36 @@ data "dns_a_record_set" "hacker_news" {
 
 output "hacker_news_addrs" {
   value = join(",", data.dns_a_record_set.hacker_news.addrs)
+}
+
+////* Use a for expression to pull a distict set of values out of a map *////
+variable "disks" {
+
+  default = {
+
+    "jenkins-data-disk" = {
+      name                = "disk1"
+      resource_group_name = "rg1"
+    },
+    "nexus-data-disk" = {
+      name                = "disk2"
+      resource_group_name = "rg2"
+    },
+    "twistlock-data-disk" = {
+      name                = "disk3"
+      resource_group_name = "rg2"
+    }
+
+  }
+}
+
+locals {
+  // note that can do 'for key, value' below if you want to reference the key
+  disk_rgs = distinct([for value in var.disks : "${value.resource_group_name}"])
+}
+
+output "resource_groups" {
+
+  value = local.disk_rgs
+
 }
