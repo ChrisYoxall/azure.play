890f7bcdd41413a30874539dfdd55342a0526614
Chris Yoxall <chris.yoxall@tower.co.nz>
2024-03-24 09:06:55 +1300

    Update type

--- terraform/storage/storage_account_v2/modules/storage_account/variables.tf
+++ terraform/storage/storage_account_v2/modules/storage_account/variables.tf
@@ -1,11 +1,4 @@
 variable "config" {
   description = "Configuration for the storage accounts"
-  default = {
-    storage_account_one = {
-      location = "Australia East"
-    },
-    storage_account_two = {
-      location = "Australia East"
-    }
-  }
+  type        = any
 }
